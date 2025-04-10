b45cd7cc084e52acd6a21ea0fa69860f4c1281a9
SergioPleguezuelosLorente <sergio.p.lorente@gmail.com>
2025-04-10 13:34:11 +0200

    added user interface template

---
+++ src/main/java/com/escuela/colegio/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.escuela.colegio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value={"", "/", "/home"})
+    public String homepage(Model model) {
+        model.addAttribute("usuario", "Luisma");
+        return "home.html";
+    }
+
+}
