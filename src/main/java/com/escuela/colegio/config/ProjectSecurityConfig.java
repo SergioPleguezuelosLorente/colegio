2d686c0a647e1f3c8ece716e922e4060147c6b15
SergioPleguezuelosLorente <sergio.p.lorente@gmail.com>
2025-04-17 11:03:43 +0200

    Add login and logout

--- src/main/java/com/escuela/colegio/config/ProjectSecurityConfig.java
+++ src/main/java/com/escuela/colegio/config/ProjectSecurityConfig.java
@@ -2,10 +2,11 @@ package com.escuela.colegio.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,6 +16,7 @@ public class ProjectSecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.csrf((csrf) -> csrf.disable())
                 .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/dashboard").authenticated()
                         .requestMatchers("/", "/home").permitAll()
                         .requestMatchers("/holidays/**").permitAll()
                         .requestMatchers("/contact").permitAll()
@@ -22,10 +24,27 @@ public class ProjectSecurityConfig {
                         .requestMatchers("/courses").permitAll()
                         .requestMatchers("/about").permitAll()
                         .requestMatchers("/assets/**").permitAll()
+                        .requestMatchers("/login").permitAll()
                 )
-                .formLogin(Customizer.withDefaults())
+                .formLogin(loginConfigurer -> loginConfigurer.loginPage("login")
+                        .defaultSuccessUrl("/dashboard").failureForwardUrl("/login?error=true").permitAll())
+                .logout(logoutConfigurer -> logoutConfigurer.logoutSuccessUrl("login?logout=true").invalidateHttpSession(true))
                 .httpBasic(Customizer.withDefaults());
         return http.build();
     }
-
+    //TODO reemplazar esto por usuarios en db
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("123456")
+                .roles("USER", "ADMIN")
+                .build();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("123456")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
 }
