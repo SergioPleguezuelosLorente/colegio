af51b8a25fc2ceed9e6e44e116bbe4dcddaeccf6
SergioPleguezuelosLorente <sergio.p.lorente@gmail.com>
2025-04-29 12:20:21 +0200

    changes to base entity and add AuditAwareImpl

--- src/main/java/com/escuela/colegio/ColegioApplication.java
+++ src/main/java/com/escuela/colegio/ColegioApplication.java
@@ -3,10 +3,12 @@ package com.escuela.colegio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories("com.escuela.colegio.Repository")
 @EntityScan("com.escuela.colegio.Model")
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @SpringBootApplication
 public class ColegioApplication {
 
--- src/main/java/com/escuela/colegio/Model/BaseEntity.java
+++ src/main/java/com/escuela/colegio/Model/BaseEntity.java
@@ -1,24 +1,34 @@
 package com.escuela.colegio.Model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
-
+    @CreatedDate
     @Column(name="created_at")
     private LocalDateTime createdAt;
 
+    @CreatedBy
     @Column(name="created_by")
     private String createdBy;
 
+    @LastModifiedDate
     @Column(name="updated_at")
     private LocalDateTime updatedAt;
 
+    @LastModifiedBy
     @Column(name="updated_by")
     private String updatedBy;
 
