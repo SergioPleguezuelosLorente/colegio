580104895f78bdd93b5bc1011606857c01054149
SergioPleguezuelosLorente <sergio.p.lorente@gmail.com>
2025-05-12 17:38:36 +0200

    AutoFill Profile data

--- src/main/java/com/escuela/colegio/controller/ProfileController.java
+++ src/main/java/com/escuela/colegio/controller/ProfileController.java
@@ -1,5 +1,6 @@
 package com.escuela.colegio.controller;
 
+import com.escuela.colegio.model.Address;
 import com.escuela.colegio.model.Person;
 import com.escuela.colegio.model.Profile;
 import jakarta.servlet.http.HttpSession;
@@ -9,12 +10,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class ProfileControllar {
+public class ProfileController {
 
     @RequestMapping("/displayProfile")
     public ModelAndView displayMessages(Model model, HttpSession session) {
         Person person = (Person) session.getAttribute("loggedInPerson");
         Profile profile = new Profile();
+        profile.setName(person.getName());
+        profile.setMobileNumber(person.getMobileNumber());
+        profile.setEmail(person.getEmail());
+        Address address = person.getAddress();
+        if(address != null && address.getAddressId()>0){
+            profile.setAddress1(address.getAddress1());
+            profile.setAddress2(address.getAddress2());
+            profile.setCity(address.getCity());
+            profile.setState(address.getState());
+            profile.setZipCode(address.getZipCode());
+        }
         ModelAndView modelAndView = new ModelAndView("profile.html");
         modelAndView.addObject("profile", profile);
         return modelAndView;
--- src/main/java/com/escuela/colegio/model/Address.java
+++ src/main/java/com/escuela/colegio/model/Address.java
@@ -23,9 +23,61 @@ public class Address {
     @Size(min = 5, message = "State must be at least 5 characters long")
     private String state;
 
+    @NotBlank(message = "City must not be blank")
+    @Size(min = 5, message = "City must be at least 5 characters long")
+    private String city;
+
+
     @NotBlank(message = "Zip Code must be blank")
     @Pattern(regexp = "(^$|[0-9]{5})", message = "Zip Code must be 5 digits")
     @Column(name = "zip_code")
     private String zipCode;
 
+    public int getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(int addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
 }
