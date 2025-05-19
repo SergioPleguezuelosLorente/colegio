52807d115cc5dfd1d09c380ad799797040f85615
SergioPleguezuelosLorente <sergio.p.lorente@gmail.com>
2025-05-19 17:37:22 +0200

    refactorizate code for better reading

--- src/main/java/com/escuela/colegio/repository/CoursesRepository.java
+++ src/main/java/com/escuela/colegio/repository/CoursesRepository.java
@@ -4,7 +4,6 @@ import com.escuela.colegio.model.Courses;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 @Repository
