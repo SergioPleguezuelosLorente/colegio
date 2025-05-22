036f3eb10b75af85485c48ecc69654580026f91d
SergioPleguezuelosLorente <sergio.p.lorente@gmail.com>
2025-05-22 11:10:34 +0200

    new ContactRestController method deleteMsg

--- src/main/java/com/escuela/colegio/rest/ContactRestController.java
+++ src/main/java/com/escuela/colegio/rest/ContactRestController.java
@@ -1,5 +1,6 @@
 package com.escuela.colegio.rest;
 
+import com.escuela.colegio.config.Constants;
 import com.escuela.colegio.model.Contact;
 import com.escuela.colegio.model.Response;
 import com.escuela.colegio.repository.ContactRepository;
@@ -7,11 +8,15 @@ import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(path = "/api/contact")
@@ -39,7 +44,7 @@ public class ContactRestController {
 
     @PostMapping("/saveMsg")
     public ResponseEntity<Response> saveMsg(@RequestHeader("invocationFrom") String invocationFrom,
-                                            @Valid @RequestBody Contact contact){
+                                            @Valid @RequestBody Contact contact) {
         log.info(String.format("Header invocationFrom = %s", invocationFrom));
         contactRepository.save(contact);
         Response response = new Response();
@@ -51,4 +56,19 @@ public class ContactRestController {
                 .body(response);
 
     }
+
+    @DeleteMapping("/deleteMsg")
+    public ResponseEntity<Response> deleteMsg(RequestEntity<Contact> requestEntity) {
+        HttpHeaders headers = requestEntity.getHeaders();
+        headers.forEach((key, value) -> {
+            log.info(String.format("Header '%s' = %s", key, value.stream().collect(Collectors.joining("|"))));
+        });
+        Contact contact = requestEntity.getBody();
+        contactRepository.deleteById(contact.getContactId());
+        Response response = new Response();
+        response.setStatusCode("200");
+        response.setStatusCode("Message succesfully deleted");
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
 }
