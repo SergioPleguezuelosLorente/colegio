9d716b0379b34fc7a928bdc696da162695c066bc
SergioPleguezuelosLorente <sergio.p.lorente@gmail.com>
2025-05-27 13:35:38 +0200

    add spring boot data rest and adapt the project

--- src/main/java/com/escuela/colegio/config/ProjectSecurityConfig.java
+++ src/main/java/com/escuela/colegio/config/ProjectSecurityConfig.java
@@ -22,6 +22,7 @@ public class ProjectSecurityConfig {
                         .requestMatchers("/closeMsg/**").hasRole("ADMIN")
                         .requestMatchers("/", "/home").permitAll()
                         .requestMatchers("/holidays/**").permitAll()
+                        .requestMatchers("/profile/**").permitAll()
                         .requestMatchers("/contact").permitAll()
                         .requestMatchers("/saveMsg").permitAll()
                         .requestMatchers("/courses").permitAll()
--- src/main/java/com/escuela/colegio/controller/ProfileController.java
+++ src/main/java/com/escuela/colegio/controller/ProfileController.java
@@ -9,7 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@Controller("profileControllerBean")
 public class ProfileController {
 
     @RequestMapping("/displayProfile")
